a100a8fdd344b5a7d101ec76cff831631669ee5b
Ankit Sharma <ankitsharma290101@gmail.com>
2023-10-19 10:08:36 +0530

    Router+Components Layout

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo">Milan Dairy</div>
+      <nav>
+        <ul className="nav-links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
